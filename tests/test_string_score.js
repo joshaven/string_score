6ae318ca67f5579170aaa203415bfb4467fa5cb6
Joshaven Potter <yourtech@gmail.com>
2009-06-20 01:42:28 -0400

    Uncomment benchmark tests

--- tests/test_string_score.js
+++ tests/test_string_score.js
@@ -50,41 +50,41 @@ $(document).ready(function(){
     ok("Hillsdale".score("hi") > "Chippewa".score("hi"));
   });
   
-  // module('Benchmark');
-  // test('Expand to see time to score', function(){
-  //   var itterations = 5000;
-  // 
-  //   var start1 = new Date().valueOf();
-  //   for(i=itterations;i>0;i--){ "hello world".score("h"); }
-  //   var end1 = new Date().valueOf();
-  //   var t1=end1-start1;
-  //   ok(true, t1 + ' miliseconds to do '+itterations+' itterations of "hello world".score("h")');
-  //   
-  //   var start2 = new Date().valueOf();
-  //   for(i=itterations;i>0;i--){ "hello world".score("hw"); }
-  //   var end2 = new Date().valueOf();
-  //   var t2=end2-start2;
-  //   ok(true, t2 + ' miliseconds to do '+itterations+' itterations of "hello world".score("hw")');
-  // 
-  //   var start3 = new Date().valueOf();
-  //   for(i=itterations;i>0;i--){ "hello world".score("hello world"); }
-  //   var end3 = new Date().valueOf();
-  //   var t3=end3-start3;
-  //   ok(true, t3 + ' miliseconds to do '+itterations+' itterations of "hello world".score("hello world")');
-  //   
-  //   var start4 = new Date().valueOf();
-  //   for(i=itterations;i>0;i--){ "hello any world that will listen".score("hlo wrdthlstn"); }
-  //   var end4 = new Date().valueOf();
-  //   var t4=end4-start4;
-  //   ok(true, t4 + ' miliseconds to do '+itterations+' itterations of "hello any world that will listen".score("hlo wrdthlstn")');
-  // 
-  //   var start5 = new Date().valueOf();
-  //   for(i=itterations;i>0;i--){ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".score("Lorem i dor coecadipg et, Duis aute irure dole nulla. qui ofa mot am l"); }
-  //   var end5 = new Date().valueOf();
-  //   var t5=end5-start5;
-  //   ok(true, t5 + ' miliseconds to do '+itterations+' itterations of 446 character string scoring a 70 character match');
-  // 
-  //   
-  //   ok(true, 'score (smaller is faster): '+ (t1+t2+t3+t4+t5)/5);
-  // });
+  module('Benchmark');
+      test('Expand to see time to score', function(){
+        var iterations = 4000;
+      
+        var start1 = new Date().valueOf();
+        for(i=iterations;i>0;i--){ "hello world".score("h"); }
+        var end1 = new Date().valueOf();
+        var t1=end1-start1;
+        ok(true, t1 + ' miliseconds to do '+iterations+' iterations of "hello world".score("h")');
+        
+        var start2 = new Date().valueOf();
+        for(i=iterations;i>0;i--){ "hello world".score("hw"); }
+        var end2 = new Date().valueOf();
+        var t2=end2-start2;
+        ok(true, t2 + ' miliseconds to do '+iterations+' iterations of "hello world".score("hw")');
+      
+        var start3 = new Date().valueOf();
+        for(i=iterations;i>0;i--){ "hello world".score("hello world"); }
+        var end3 = new Date().valueOf();
+        var t3=end3-start3;
+        ok(true, t3 + ' miliseconds to do '+iterations+' iterations of "hello world".score("hello world")');
+        
+        var start4 = new Date().valueOf();
+        for(i=iterations;i>0;i--){ "hello any world that will listen".score("hlo wrdthlstn"); }
+        var end4 = new Date().valueOf();
+        var t4=end4-start4;
+        ok(true, t4 + ' miliseconds to do '+iterations+' iterations of "hello any world that will listen".score("hlo wrdthlstn")');
+      
+        var start5 = new Date().valueOf();
+        for(i=iterations;i>0;i--){ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".score("Lorem i dor coecadipg et, Duis aute irure dole nulla. qui ofa mot am l"); }
+        var end5 = new Date().valueOf();
+        var t5=end5-start5;
+        ok(true, t5 + ' miliseconds to do '+iterations+' iterations of 446 character string scoring a 70 character match');
+      
+        
+        ok(true, 'score (smaller is faster): '+ (t1+t2+t3+t4+t5)/5);
+      });
 });
