64159a52c4a70148b123ca3402da5edfb889d7e8
Joshaven Potter <yourtech@gmail.com>
2009-06-20 00:05:07 -0400

    Implement better scoring methods:  weighted scores for: (Acronym, Consecutive matches & First letter matches) Adjust score weights

--- string_score.js
+++ string_score.js
@@ -1,53 +1,59 @@
-// string_score - Quicksilver Like Score
-// 
-// This is a rewrite of A port of the Quicksilver string ranking algorithm.  
-// Special thanks to 'Lachie Cox' for inspiration
-// 
-// "hello world".score("axl") //=> 0
-// "hello world".score("ow")  //=> 0.18181818181818182 
-//
-// "hello world".score("h")           //=>0.09090909090909091
-// "hello world".score("he")          //=>0.18181818181818182
-// "hello world".score("hel")         //=>0.2727272727272727
-// "hello world".score("hell")        //=>0.36363636363636365
-// "hello world".score("hello")       //=>0.45454545454545453
-// ...
-// "hello world".score("helloworld")  //=>0.90909090909090913
-// "hello world".score("hello worl")  //=>0.9090909090909091
-// "hello world".score("hello world") //=> 1
-//
-// // Score is better when the strings are closer
-// 'Hello'.score('h') //=>0.13999999999999999
-// 'He'.score('h')    //=>0.35
-//
-// // Same case matches better then wrong case
-// 'Hello'.score('h') //=>0.13999999999999999
-// 'Hello'.score('H') //=>0.2
-//
-// The MIT License
-// 
-// Copyright (c) 2008 Joshaven Potter <yourtech@gmail.com>
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+/*
+ * string_score - Quicksilver Like Score
+ * 
+ * This javascript will score a string against another string. It began as a rewrite of A port of the 
+ * Quicksilver string ranking algorithm (quicksilver.js aka qs_score.js).  The final product only
+ * contains a few characters from the original algorithm
+ *
+ * Special thanks to 'Lachie Cox' for inspiration
+ * 
+ * Examples:
+ * "hello world".score("axl") //=> 0
+ * "hello world".score("ow")  //=> 0.18181818181818182 
+ *
+ * "hello world".score("h")           //=>0.09090909090909091
+ * "hello world".score("he")          //=>0.18181818181818182
+ * "hello world".score("hel")         //=>0.2727272727272727
+ * "hello world".score("hell")        //=>0.36363636363636365
+ * "hello world".score("hello")       //=>0.45454545454545453
+ * ...
+ * "hello world".score("helloworld")  //=>0.90909090909090913
+ * "hello world".score("hello worl")  //=>0.9090909090909091
+ * "hello world".score("hello world") //=> 1
+ *
+ * // Score is better when the strings are closer
+ * 'Hello'.score('h') //=>0.13999999999999999
+ * 'He'.score('h')    //=>0.35
+ *
+ * // Same case matches better then wrong case
+ * 'Hello'.score('h') //=>0.13999999999999999
+ * 'Hello'.score('H') //=>0.2
+ *
+ *
+ *
+ * The MIT License
+ * 
+ * Copyright (c) 2008 Joshaven Potter <yourtech@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
 
-
-// String.prototype.score = function(abbreviation,offset) {
 String.prototype.score = function(abbr) {
   function charFlopCase(c) { u = c.toUpperCase(c); return c == u ? c.toLowerCase(c) : u; }
   if(this == abbr) return 1.0;
@@ -57,21 +63,31 @@ String.prototype.score = function(abbr) {
   var string_length = string.length;
 
   for(var i=0,len=abbr_length;i<len;i++){ // Walk through abbreviation
-    var loc = string.indexOf(abbr.charAt(i));
-    if(loc > -1){ // proper case match
-      scores = scores.concat(1.0);
-      string = string.substring(loc,string_length); // left-trim up to the match
+    var location = string.indexOf(abbr.charAt(i));
+    if(location > -1){ // proper case match
+      scores = scores.concat(0.6); // set score when exact match
     } else {
-      loc = string.indexOf(charFlopCase(abbr.charAt(i)));
-      if(loc > -1){
-        scores = scores.concat(0.7);
-        string = string.substring(loc,string_length); // left-trim up to the match
+      location = string.indexOf(charFlopCase(abbr.charAt(i)));
+      if(location > -1){
+        scores = scores.concat(0.5); // set score when match but wrong case
       } else {
-        return 0
+        return 0;
       }
     }
+    
+    // Consecutive Letter Bonus
+    // increate the score when matching first char of string OR when matching first letter of a word
+    if(location===0) scores[scores.length-1]+=0.4;
+
+    // Acronym Bonus
+    // Weighting Logic: Typeing the first letter of an acronym is at most as if you preceeded it by two perfect letter matches
+    if(string.charAt(location-1)===' ')
+      scores[scores.length-1] += 0.4 * Math.min(location, 5); // cap bonus at 0.4 * 5  
+
+    // Left Trim the already matched part of the string (forces sequential matches)
+    string = string.substring(location+1 ,string_length); 
   }
 
-  for(var i=0,sum=0,l=abbr_length;i<l;i++) sum+=scores[i];
+  for(i=0,sum=0,l=abbr_length;i<l;i++) sum+=scores[i];
   return sum/this.length;
 };
--- tests/test_string_score.js
+++ tests/test_string_score.js
@@ -26,54 +26,65 @@ $(document).ready(function(){
     ok('Hello World'.score('H')<'Hello World'.score('He'));
   });
   
-  module('Advanced Scoreing Methods (not implemented)');
+  test('should match first matchable letter regardless of case', function(){
+    ok("Hillsdale Michigan".score("himi")>0);
+  });
+  
+  module('Advanced Scoreing Methods');
   test('consecutive letter bonus', function(){
+    expect(1);
     ok('Hello World'.score('Hel') > 'Hello World'.score('Hld'));
   });
   
   test('Acronym bonus', function(){
+    expect(5);
     ok('Hello World'.score('HW') > 'Hello World'.score('Hl'));
+    ok('yet another Hello World'.score('yaHW') > 'Hello World'.score('yet another'));
+    ok("Hillsdale Michigan".score("HiMi") > "Chippewa Michigan".score("Chipp"));
+    ok("Hillsdale Michigan".score("HiMi") > "Hillsdale Michigan".score("Hills"));
+    ok("Hillsdale Michigan".score("HiMi") < "Hillsdale Michigan".score("Hillsda"));
   });
   
   test('Beginning of string bonus', function(){
+    expect(1);
     ok("Hillsdale".score("hi") > "Chippewa".score("hi"));
   });
   
-  module('Benchmark');
-  test('Expand to see time to score', function(){
-    var itterations = 5000;
-  
-    var start1 = new Date().valueOf();
-    for(i=itterations;i>0;i--){ "hello world".score("h"); }
-    var end1 = new Date().valueOf();
-    var t1=end1-start1;
-    ok(true, t1 + ' miliseconds to do '+itterations+' itterations of "hello world".score("h")');
-    
-    var start2 = new Date().valueOf();
-    for(i=itterations;i>0;i--){ "hello world".score("hw"); }
-    var end2 = new Date().valueOf();
-    var t2=end2-start2;
-    ok(true, t2 + ' miliseconds to do '+itterations+' itterations of "hello world".score("hw")');
-  
-    var start3 = new Date().valueOf();
-    for(i=itterations;i>0;i--){ "hello world".score("hello world"); }
-    var end3 = new Date().valueOf();
-    var t3=end3-start3;
-    ok(true, t3 + ' miliseconds to do '+itterations+' itterations of "hello world".score("hello world")');
-    
-    var start4 = new Date().valueOf();
-    for(i=itterations;i>0;i--){ "hello any world that will listen".score("hlo wrdthlstn"); }
-    var end4 = new Date().valueOf();
-    var t4=end4-start4;
-    ok(true, t4 + ' miliseconds to do '+itterations+' itterations of "hello any world that will listen".score("hlo wrdthlstn")');
-  
-    var start5 = new Date().valueOf();
-    for(i=itterations;i>0;i--){ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".score("Lorem i dor coecadipg et, Duis aute irure dole nulla. qui ofa mot am l"); }
-    var end5 = new Date().valueOf();
-    var t5=end5-start5;
-    ok(true, t5 + ' miliseconds to do '+itterations+' itterations of 446 character string scoreing a 70 character match');
-  
-    
-    ok(true, 'score (smaller is faster): '+ (t1+t2+t3+t4+t5)/5);
-  });
+  // module('Benchmark');
+  // test('Expand to see time to score', function(){
+  //   var itterations = 5000;
+  // 
+  //   var start1 = new Date().valueOf();
+  //   for(i=itterations;i>0;i--){ "hello world".score("h"); }
+  //   var end1 = new Date().valueOf();
+  //   var t1=end1-start1;
+  //   ok(true, t1 + ' miliseconds to do '+itterations+' itterations of "hello world".score("h")');
+  //   
+  //   var start2 = new Date().valueOf();
+  //   for(i=itterations;i>0;i--){ "hello world".score("hw"); }
+  //   var end2 = new Date().valueOf();
+  //   var t2=end2-start2;
+  //   ok(true, t2 + ' miliseconds to do '+itterations+' itterations of "hello world".score("hw")');
+  // 
+  //   var start3 = new Date().valueOf();
+  //   for(i=itterations;i>0;i--){ "hello world".score("hello world"); }
+  //   var end3 = new Date().valueOf();
+  //   var t3=end3-start3;
+  //   ok(true, t3 + ' miliseconds to do '+itterations+' itterations of "hello world".score("hello world")');
+  //   
+  //   var start4 = new Date().valueOf();
+  //   for(i=itterations;i>0;i--){ "hello any world that will listen".score("hlo wrdthlstn"); }
+  //   var end4 = new Date().valueOf();
+  //   var t4=end4-start4;
+  //   ok(true, t4 + ' miliseconds to do '+itterations+' itterations of "hello any world that will listen".score("hlo wrdthlstn")');
+  // 
+  //   var start5 = new Date().valueOf();
+  //   for(i=itterations;i>0;i--){ "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".score("Lorem i dor coecadipg et, Duis aute irure dole nulla. qui ofa mot am l"); }
+  //   var end5 = new Date().valueOf();
+  //   var t5=end5-start5;
+  //   ok(true, t5 + ' miliseconds to do '+itterations+' itterations of 446 character string scoreing a 70 character match');
+  // 
+  //   
+  //   ok(true, 'score (smaller is faster): '+ (t1+t2+t3+t4+t5)/5);
+  // });
 });
