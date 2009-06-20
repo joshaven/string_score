191e8b08f3b1fed3e68c8d3d1a75cc586b345708
Joshaven Potter <yourtech@gmail.com>
2009-06-20 01:41:18 -0400

    Rework case scoring code to find first match regardless of case & score approprately

--- string_score.js
+++ string_score.js
@@ -15,7 +15,7 @@
  *
  * The MIT License
  * 
- * Copyright (c) 2008 Joshaven Potter <yourtech@gmail.com>
+ * Copyright (c) 2009 Joshaven Potter <yourtech@gmail.com>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@
 */
 
 String.prototype.score = function(abbr) {
-  function charFlopCase(c) { u = c.toUpperCase(c); return c == u ? c.toLowerCase(c) : u; }
+  function lowestValidIndex(a,b){ var min=Math.min(a,b); if(min>-1)return min; return Math.max(a,b); }
   if(this == abbr) return 1.0;
   var scores = [];
   var abbr_length = abbr.length;
@@ -45,27 +45,27 @@ String.prototype.score = function(abbr) {
   var string_length = string.length;
 
   for(var i=0,len=abbr_length;i<len;i++){ // Walk through abbreviation
-    var location = string.indexOf(abbr.charAt(i));
-    if(location > -1){ // proper case match
-      scores = scores.concat(0.6); // set score when exact match
-    } else {
-      location = string.indexOf(charFlopCase(abbr.charAt(i)));
-      if(location > -1){
-        scores = scores.concat(0.5); // set score when match but wrong case
-      } else {
-        return 0;
-      }
-    }
+    // var match_char = abbr.charAt(i);
+    
+    // find the first case insensitive match of a charactor
+    var location = lowestValidIndex(string.indexOf(abbr.charAt(i).toLowerCase()), string.indexOf(abbr.charAt(i).toUpperCase()));
+
+    if(location === -1) return 0; // Bail out if no match is found
+    
+    scores.push(0.5); // set base score
+
+    // case bonus
+    if(string.charAt(location)===abbr.charAt(i)) scores[scores.length-1]+=0.1;
     
     // Consecutive Letter Bonus
     // increate the score when matching first char of string OR when matching first letter of a word
     if(location===0) scores[scores.length-1]+=0.4;
-
+    
     // Acronym Bonus
     // Weighting Logic: Typeing the first letter of an acronym is at most as if you preceeded it by two perfect letter matches
     if(string.charAt(location-1)===' ')
       scores[scores.length-1] += 0.4 * Math.min(location, 5); // cap bonus at 0.4 * 5  
-
+      
     // Left Trim the already matched part of the string (forces sequential matches)
     string = string.substring(location+1 ,string_length); 
   }
