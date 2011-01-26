e271e721cbedc1714f843dc22ed70fcf9388d708
Gora Khargosh <gora.khargosh@gmail.com>
2011-01-26 09:22:27 +0530

    Adds minified version
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- string_score_min.js
+++ string_score_min.js
@@ -1,12 +1,12 @@
- /*
-  * string_score.js Javascript Plugin > Quicksilver Like Score
+ /* string_score.js: Quicksilver-like string scoring
   * http://github.com/joshaven/string_score/tree/master
   *
-  * Copyright (c) 2009 Joshaven Potter <yourtech@gmail.com>
+  * Copyright (C) 2009 Joshaven Potter <yourtech@gmail.com>
+  * Copyright (C) 2010 Gora Khargosh <gora.khargosh@gmail.com>
   * Licensed under the MIT license.
   * http://www.opensource.org/licenses/mit-license.php
   *
-  * Date: 2009-06-20
-  * Version 1.0
+  * Date: 2011-01-26
+  * Version 1.1b
   */
-String.prototype.score=function(d){function e(a,b){var m=Math.min(a,b);if(m>-1)return m;return Math.max(a,b);}if(this==d)return 1.0;var f=[];var g=d.length;var h=this;var j=h.length;for(var i=0,k=g;i<k;i++){var location=e(h.indexOf(d.charAt(i).toLowerCase()),h.indexOf(d.charAt(i).toUpperCase()));if(location===-1)return 0;f.push(0.5);if(h.charAt(location)===d.charAt(i))f[f.length-1]+=0.1;if(location===0)f[f.length-1]+=0.4;if(h.charAt(location-1)==='\x20')f[f.length-1]+=0.4*Math.min(location,5);h=h.substring(location+1,j);}for(i=0,sum=0,l=g;i<l;i++)sum+=f[i];return sum/this.length;};
+String.prototype.score=function(l){if(this==l){return 1}var r=0,p=l.length,g=this,o=g.length,n=false,k=0,s=0,q=0,e=0;for(var f=0,b=0,m=0,j="",a="",d="",h;f<p;++f){j=l.charAt(f);a=g.indexOf(j.toLowerCase());d=g.indexOf(j.toUpperCase());h=Math.min(a,d);m=(h>-1)?h:Math.max(a,d);if(m===-1){return 0}b=0.1;if(g.charAt(m)===j){b+=0.1}if(m===0){b+=0.8;if(f===0){n=true}}if(g.charAt(m-1)===" "){b+=0.8}g=g.substring(m+1,o);r+=b}k=r/p;s=p/this.length;q=k*s;e=(q+k)/2;if(n&&(e+0.1<1)){e+=0.1}return e};
